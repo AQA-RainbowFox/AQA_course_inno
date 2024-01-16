3778e2abd1e2bf4b53fbe614096ddd65b3f9a276
Дарья <noip.noname@gmail.com>
2024-01-16 22:25:24 +0300

    Homework1

---
+++ src/main/java/ru/inno/course/Dasha.java
@@ -0,0 +1,23 @@
+package ru.inno.course;
+
+public class Dasha {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+    // типыданных
+        // 1. что я могу хранить?
+        // 2.
+        
+        boolean isEmailValid = true; //false
+
+
+
+
+
+
+
+    }
+
+
+}
